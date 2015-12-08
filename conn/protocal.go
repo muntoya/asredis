dba6bda67a8b50e3dbcf87730724b5e2c5cdcbfb
yaoxing <www.yaoxing.com@gmail.com>
2015-12-08 20:56:34 +0800

    增加client结构字段

--- conn/protocal.go
+++ conn/protocal.go
@@ -1,1 +1,13 @@
 package conn
+
+const (
+	cr_byte    byte = byte('\r')
+	lf_byte         = byte('\n')
+	space_byte      = byte(' ')
+	err_byte        = byte('-')
+	ok_byte         = byte('+')
+	count_byte      = byte('*')
+	size_byte       = byte('$')
+	num_byte        = byte(':')
+	true_byte       = byte('1')
+)
